1763aee1d9bdeeafffc6972939ac175825f4388a
emmanuelmuniz <munizadaroemmanuel@gmail.com>
2022-04-04 17:06:06 -0300

    First Commit

---
+++ src/main/java/com/sistemas/operaciones/controllers/OperacionController.java
@@ -0,0 +1,44 @@
+package com.sistemas.operaciones.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OperacionController {
+
+	@Autowired
+	private RestTemplate clienteRest;
+	
+	@GetMapping("/sumar-usa")
+	public String sumarUsa(@RequestParam float a, @RequestParam float b) {
+		
+		String resultado = Float.toString(a + b);
+		
+		return clienteRest.getForObject("https://resultados-sd-usa.herokuapp.com/resultado/" + resultado, String.class);
+	}
+	
+	@GetMapping("/restar-usa")
+	public String restarUsa(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a - b);
+		
+		return clienteRest.getForObject("https://resultados-sd-usa.herokuapp.com/resultado/" + resultado, String.class);
+	}
+
+	@GetMapping("/sumar-eu")
+	public String sumarEu(@RequestParam float a, @RequestParam float b) {
+
+		String resultado = Float.toString(a + b);
+
+		return clienteRest.getForObject("https://resultados-sd-eu.herokuapp.com/resultado/" + resultado, String.class);
+	}
+
+	@GetMapping("/restar-eu")
+	public String restarEu(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a - b);
+
+		return clienteRest.getForObject("https://resultados-sd-eu.herokuapp.com/resultado/" + resultado, String.class);
+	}
+}
